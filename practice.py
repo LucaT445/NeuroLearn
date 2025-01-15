72598680af27f7079d04619ea097e5f0f8bf6a59
LucaT445 <luca.thilgen@gmail.com>
2025-01-15 11:23:48 -0800

    Added green background

--- practice.py
+++
@@ -1,5 +0,0 @@
-from brain_educational_tool import get_brain_data
-
-file_path = 'brain_regions.json'
-brain_data = get_brain_data(file_path)
-print(brain_data)
--- visualization.py
+++ visualization.py
@@ -2,42 +2,41 @@ import trimesh
 import numpy as np
 import plotly.graph_objects as go
 
-# Path to your brain STL file
 stl_path = r"C:\Users\lucat\Downloads\brain.stl"
 
-# Load the STL file using trimesh
 mesh = trimesh.load_mesh(stl_path)
 
-# Extract vertices and faces from the mesh
+# Gets vertices and face data from the brain mesh
 vertices = mesh.vertices
 faces = mesh.faces
 
-# Set all vertices to black
-colors = np.zeros((len(vertices), 3))  # Default black for all vertices
+colors = np.zeros((len(vertices), 3))  
 
 
-# Create the 3D mesh visualization
+# Creates the 3D mesh visualization
 fig = go.Figure(data=[go.Mesh3d(
     x=vertices[:, 0], y=vertices[:, 1], z=vertices[:, 2],
     i=faces[:, 0], j=faces[:, 1], k=faces[:, 2],
-    vertexcolor=colors,  # Assign vertex colors
-    opacity=0.25  # semi-opaque
+    vertexcolor=colors,  
+    opacity=0.25  # Low opacity is used to view the internal regions of the brain
 )])
 
-# Add layout details
 fig.update_layout(
-    title="3D Brain Visualization",
+    title="Brain Visualization",
     scene=dict(
         xaxis_title="X-axis",
         yaxis_title="Y-axis",
         zaxis_title="Z-axis",
         camera=dict(
             eye=dict(x=0, y=0, z=2),  # Position camera above brain for top view
-            up=dict(x=0, y=1, z=0)     # Orient "up" direction
+            up=dict(x=0, y=1, z=0)     
         ),
-        aspectmode='data'  # Equal aspect ratio
-    )
+        aspectmode='data',  
+        bgcolor="forestgreen"
+    ),
+    paper_bgcolor="forestgreen"
+
 )
 
-# Show the visualization
+
 fig.show()
